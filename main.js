d387bbce02e0a91ffee98b6ebe30c6fad81e8d22
Sushrita-Sharma <117659917+sushritasharma@users.noreply.github.com>
2024-02-11 19:07:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+const populate= async (value, currency) =>{
+    let myStr="";
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_NFjF8ejahdbgtrhWIDVCQjpq7QVRveJR1mBRZVBh&base_currency=" + currency;
+    let response = await fetch(url)
+    let rJson = await response.json();
+    document.querySelector(".output").style.display="block";
+    
+    for(let key of  Object.keys(rJson["data"])){
+        myStr += `
+        <tr>
+            <td>${key}</td>
+            <td>${key, rJson["data"][key]["code"]}</td>
+            <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
+        </tr>
+        `
+       
+    }
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
+
+}
+
+
+const btn = document.querySelector('button');
+
+
+btn.addEventListener("click", (e)=> {
+    e.preventDefault();
+    console.log("button is clicked!");
+
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+    const currency = document.querySelector("select[name='currency']").value;
+    populate(value, currency);
+})
+
+
+
